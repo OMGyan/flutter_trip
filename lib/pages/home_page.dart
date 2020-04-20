3b3208af0f816b1e8d248a2667d5094bb8bc2422
kevin <1768695841@qq.com>
2020-04-20 17:05:40 +0800

    app首页框架搭建

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget{
+  @override
+  _HomePageState createState() => _HomePageState();
+
+}
+
+class _HomePageState extends State<HomePage>{
+
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body:Center(
+        child: Text('首页')
+      )
+    );
+  }
+
+}
---
+++ lib/pages/pages_index.dart
@@ -0,0 +1,4 @@
+export 'home_page.dart';
+export 'my_page.dart';
+export 'search_page.dart';
+export 'travel_page.dart';
