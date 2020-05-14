988c76443012ba67982a73874b3ae8a33013de68
yanxun <1768695841@qq.com>
2020-05-14 17:47:07 +0800

    dart端调用native方法管理类

---
+++ lib/plugin/asr_manager.dart
@@ -0,0 +1,19 @@
+
+
+import 'package:flutter/services.dart';
+
+class AsrManager{
+  static const MethodChannel _channel = const MethodChannel('asr_plugin');
+  ///开始录音
+  static Future<String> start({Map params}) async {
+    await _channel.invokeMethod('start',params??{});
+  }
+  ///停止录音
+  static Future<String> stop() async {
+    await _channel.invokeMethod('stop');
+  }
+  ///取消录音
+  static Future<String> cancel() async {
+    await _channel.invokeMethod('cancel');
+  }
+}
